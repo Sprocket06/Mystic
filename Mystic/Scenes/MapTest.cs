470e5e7acb55ddbcf04d3f639e3489f3fa60449b
Sprocket06 <24927632+Sprocket06@users.noreply.github.com>
2021-05-13 18:16:36 -0400

    fix render bug, add Map.DrawRect

--- Mystic/Scenes/MapTest.cs
+++ Mystic/Scenes/MapTest.cs
@@ -18,14 +18,16 @@ namespace Mystic.Scenes
         {
             tileColorMap = new Dictionary<int, Color>
             {
-                [0] = Color.White
+                [0] = Color.White,
+                [1] = Color.Black
             };
             gameWorld = new Map(new Grid(10,10), tileColorMap, 32);
+            gameWorld.DrawRect(new Vector2(2, 2), new Vector2(6, 6));
         }
 
         public override void Draw(RenderContext context)
         {
-            gameWorld.Render(context, new Vector2(0, 0));
+            gameWorld.Render(context, new Vector2(10, 10));
         }
     }
 }
--- Mystic/World/Map.cs
+++ Mystic/World/Map.cs
@@ -24,7 +24,23 @@ namespace Mystic.World
                 for(int x = 0; x < TileGrid.Size.Width; x++)
                 {
                     Vector2 tilePos = new((x * TileSize), (y * TileSize));
+                    tilePos += pos;
                     ctx.Rectangle(ShapeMode.Fill, tilePos, TileSize, TileSize, ColorMap[TileGrid[x,y]]);
+                }
+            }
+        }
+
+        public void DrawRect(Vector2 pos, Vector2 size)
+        {
+            for(float y = pos.Y; y < pos.Y + size.Y; y++)
+            {
+                for(float x = pos.X; x < pos.X + size.X; x++)
+                {
+                    if(x > TileGrid.Size.Width || y > TileGrid.Size.Height)
+                    {
+                        throw new System.Exception("Out of bounds error");
+                    }
+                    TileGrid[(int)x, (int)y] = 1;
                 }
             }
         }
